b9b8245b3134380e53af201f4b0091f701841396
Muhamad Irvan Dandung <irvandandung1@gmail.com>
2021-06-24 20:00:59 +0700

    Update: fix error handle & user registration implemented

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
-const songs = require('./api/songs/index.songs');
 const ClientError = require('./exceptions/ClientError');
+// Note
 const SongsService = require('./service/postgres/SongService.postgres');
 const SongsValidator = require('./validator/songs');
+const songs = require('./api/songs/index.songs');
+// User
+const UsersService = require('./service/postgres/UsersService.postgres');
+const UsersValidator = require('./validator/users');
+const users = require('./api/users/index.users');
 
 const init = async () => {
   const songsService = new SongsService();
+  const usersService = new UsersService();
   const server = Hapi.Server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -20,11 +26,18 @@ const init = async () => {
   server.ext('onPreResponse', (request, h) => {
     const { response } = request;
 
-    if (response instanceof ClientError) {
-      const responseError = h.response({
-        status: 'fail',
-        message: response.message,
+    if (response instanceof Error) {
+      // console.log(response);
+      let responseError = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
       });
+      if (response instanceof ClientError) {
+        responseError = h.response({
+          status: 'fail',
+          message: response.message,
+        });
+      }
       responseError.code(response.statusCode);
       return responseError;
     }
@@ -32,13 +45,22 @@ const init = async () => {
     return response.continue || response;
   });
 
-  await server.register({
-    plugin: songs,
-    options: {
-      service: songsService,
-      validator: SongsValidator,
+  await server.register([
+    {
+      plugin: songs,
+      options: {
+        service: songsService,
+        validator: SongsValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   await server.start();
   // eslint-disable-next-line no-console
